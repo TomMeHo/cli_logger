20f8622a208ee4e4e51d1fa5a49efcf06c0be5fb
Thomas Meder <10582949+TomMeHo@users.noreply.github.com>
2021-01-28 14:22:34 +0100

    Update test_logger.py

--- test_manuell/test_logger.py
+++ test_manuell/test_logger.py
@@ -1,22 +1,24 @@
 from time import sleep
 from cli_logger import MessageClass, log, information, set_minimum_severity_level, error, debug, warning, returnCode, printProgressBar
 
-set_minimum_severity_level = MessageClass.DEBUG
+set_minimum_severity_level = MessageClass.DEBUG #default is INFORMATION. Overwrite it for example more verbous output.
 
-debug('Debug message.')
+debug('Debug message.') # use debug for lowest-level messages.
+information('Information message.') # information messages are usual output.
+print('Returncode: %s' % returnCode()) # usually, only at program end, you can check the return code if any warnings or errors have occured.
+
+warning('A warning.')
+warning('Beware!')
 print('Returncode: %s' % returnCode())
 
-information('Information message.')
+error('Error! Go home.')
 print('Returncode: %s' % returnCode())
 
-warning('A warning. Beware!')
-print('Returncode: %s' % returnCode())
-
-error('Error! GO home.')
-print('Returncode: %s' % returnCode())
-
-printProgressBar(1, 10, 'Process running [name]', 'suffix')
-sleep(2)
-printProgressBar(5, 10, 'Process running [name]', 'suffix')
-sleep(2)
-printProgressBar(10, 10, 'Process running [name]', 'suffix')
+# and there's even a progress bar included.
+printProgressBar(1, 4, 'Process running [name]', 'suffix')
+sleep(1)
+printProgressBar(2, 4, 'Process running [name]', 'suffix')
+sleep(1)
+printProgressBar(3, 4, 'Process running [name]', 'suffix')
+sleep(1)
+printProgressBar(4, 4, 'Process running [name]', 'suffix')
