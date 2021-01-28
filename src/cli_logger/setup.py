62eb5c769be3575dd9ec6214d4e337f8e7d7b67e
Thomas Meder <10582949+TomMeHo@users.noreply.github.com>
2021-01-28 20:19:48 +0100

    Update setup.py

--- src/cli_logger/setup.py
+++ src/cli_logger/setup.py
@@ -2,19 +2,25 @@ from setuptools import setup
 
 NAME = 'cli_logger'
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
       name=NAME,
-      version='0.1',
-      description='A module to log program outputs.',
+      version='1.0',
+      description='A module to pretty-print outputs for command-line programs.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/TomMeHo/cli_logger',
       author='Thomas Meder',
-      author_email='thomas@familie-meder.net',
-      license='GPL-3.0',
+      author_email='tom@tommho.net',
+      license='MIT',
       packages=[NAME],
       zip_safe=False,
       install_requires=[
             'click',
             'colorama',
             'datetime'
-      ]
+      ],
+      python_requires='>=3.6'
 )
